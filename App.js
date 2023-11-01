4fec4e41a5c75fe0492e95c5dd9d997d74e11626
VinayakWaghmode7 <vinayakwaghmode09@gmail.com>
2023-11-01 23:15:23 +0530

    Added more compoents on  swiggy webapp layout

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from 'react';
 import  ReactDOM  from 'react-dom';
 
+
 /**
 for swiggy app rough structure 
 header
@@ -17,7 +18,69 @@ Footer:-
  -Address
  */
 
- 
+//Make Header Compoent
+const  Header = () => {
+     return(
+          <div className="header">
+          {/* header contains logo and nav-items */}
+          <div className="logo-container">
+               <img className="logo" src= "https://png.pngtree.com/png-clipart/20230106/original/pngtree-simple-and-modern-food-logo-vector-design-png-image_8876455.png"/>
+          </div>
+          <div className="nav-items">
+          <ul>
+             <li>Home</li>
+             <li>About Us</li>
+             <li>Contact Us</li>
+             <li>Cart</li>
+          </ul>
+          </div>
+         </div>
+     )
+}
+
+//Restaurant-Container contains the Restrocards
+//This is react component is the at end of day javascript function.
+const RestaurantCard = (props) => {
+     // or we can do ({resName, cusines}) object distructuring...
+     //or const {resName, cusines} = props;
+     return(
+          <div className="res-card">
+          <img className="rescard-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xpcayiujcvvezicvxxhc"/>
+            <div className=" rescard-details">
+            <h3>{props.resName}</h3>
+            <h4>{props.cusines}</h4>
+            <h4>4.4</h4>
+            <h4>40 minutes</h4>
+            </div>
+          </div>
+     );
+};
+
+//This Body also React Component
+const Body= () =>{
+     return (
+          <div className='body'>
+          <div className="search">Search</div>
+          <div className="res-container">
+          <RestaurantCard resName="Barbeque" cusines="South Indian Biryani,West Indian"/>
+          <RestaurantCard resName="KFC" cusines="Biryani, North Food"/>
+          </div>
+          </div>
+     )
+}
+
+ //Make first Outerlevel compoent which contains all other components such
+ //header, body and footer components.
+ const AppLayout = () => {
+     return (
+          <div className="App">
+          {/* Render the Header or any  Compoents here because it is a outer-lebvel compoent */}
+          <Header/>
+          <Body/>
+          </div>
+     )
+ };
+
 //React.CreateElement is the core of React and it creates the heading(h1)
 //SO, we want to display the heading on to the broswer so we used a 
 //ReactDOM.createroot, whatever you like to render on the browser/display that 
@@ -42,7 +105,7 @@ const Heading = ()=>(
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading/>);
+root.render(<AppLayout/>);
 
 // above heading variable is the React element and every react element is the object and 
 // this object(heading) -> is convert it into -> the HTML(That's browser understand)
