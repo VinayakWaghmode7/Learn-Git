1677c9b020cbdfabda992d65159fa72b3638bc99
VinayakWaghmode7 <vinayakwaghmode09@gmail.com>
2023-11-02 19:52:28 +0530

    Added filter feature  on  swiggy webapp layout

--- src/Compoents/Body.js
+++ src/Compoents/Body.js
@@ -1,14 +1,27 @@
 import { RestaurantCard } from "./RestaurantCard";
 import { resObj } from "../utils/MockData";
+import { useState } from "react";
 //This Body also React Component
 export const Body= () =>{
+
+    const [restaurantList , setListOfRestaurants] = useState(resObj);
     return (
          <div className='body'>
-         <div className="search">Search</div>
+         {/* <div className="search">Search</div> */}
+         <div className="filter">
+            <button className="filter-btn"
+            onClick={() => {
+                const filterdData = restaurantList.filter(
+                    (res)=>res.info.avgRating > 4);
+                    console.log(filterdData);
+                    setListOfRestaurants(filterdData);
+            } } > Top Rated Restaurants</button>
+         </div>
          <div className="res-container">
+         
          {/* <RestaurantCard resData = {resObj}/> */}
          {
-              resObj?.map((restaurant) =>(
+              restaurantList?.map((restaurant) =>(
                    <RestaurantCard key={restaurant?.info?.id} resData={restaurant}/>
               ))
          }
